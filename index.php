7646486c57d708f88920e4c15a47da18c37f249e
Andrea Fermo <andreafermo.sd@gmail.com>
2023-10-03 11:29:04 +0200

    layout base

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    include __DIR__ . '/layout/header.php';
+?>
+<main>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edusogno</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+</main>
---
+++ layout/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edusogno</title>
+    <link rel="stylesheet" href="./assets/styles/style.css">
+</head>
+<body>
+    <header>
+        <div class="nav-container">
+            <div class="logo">
+                <?php
+                    include __DIR__ . '/../partials/logo.php'
+                ?>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
